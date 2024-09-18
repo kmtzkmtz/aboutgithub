c9ef6b267826054d3ac50501e41e5058e6944abc
kmtzkmtz <kmtzkmtzkmtzkmtzkmtz@gmail.com>
2024-09-18 18:27:46 +0300

    Закончена работа над меню и её функционалом

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleText(section) {
-    const text = section.querySelector('.function_T');
-    if (text.style.display === "none" || text.style.display === "") {
-        text.style.display = "block"; // Показываем текст
-    } else {
-        text.style.display = "none"; // Скрываем текст
-    }
-}
